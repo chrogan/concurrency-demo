605f81110b5c2e41df6fd0e7c46b39735abc5dfc
chrogan <chrogan@amazon.com>
2022-08-04 15:35:50 -0700

    Initial Commit

--- src/main/java/com/tlglearning/concurrency/Reduction.java
+++ src/main/java/com/tlglearning/concurrency/Reduction.java
@@ -1,6 +1,13 @@
 package com.tlglearning.concurrency;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Reduction implements Computation {
+  private static final int NUM_THREADS = 16;
+
+  private final Object lock = new Object();
+  private double logSum;
 
   @Override
   public double arithmeticMean(int[] data) {
@@ -9,7 +16,36 @@ public class Reduction implements Computation {
 
   @Override
   public double geometricMean(int[] data) {
-    throw new UnsupportedOperationException("Not yet implemented");
+    int slice = data.length / NUM_THREADS;
+    List<Thread> workers = new LinkedList<>();
+    for( int i =0; i < NUM_THREADS; i++){
+      workers.add(spawn(data,i * slice, (i+1) * slice));
+    }
+    for(Thread worker: workers){
+      try{
+        worker.join();
+      }catch(InterruptedException ignore){
+        //Ignore this exception.
+      }
+    }
+    return Math.exp(logSum / data.length);
+
   }
 
+  private Thread spawn(int[] data, int startIndex, int endIndex){
+    Runnable work = () -> {
+      double logSubtotal = 0;
+      for(int i = startIndex; i < endIndex; i++){
+        logSubtotal += Math.log(data[i]);
+      }
+      synchronized (lock){
+        logSum += logSubtotal;
+      }
+    };
+    Thread worker = new Thread(work);
+    worker.start();
+    return worker;
+
+
+  }
 }
